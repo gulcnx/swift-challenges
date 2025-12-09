58a8787e0d4935b132078f35d6b13dc478f19391
gülçin çetin <cgulcn777@gmail.com>
2025-12-09 15:02:46 +0300

    Add namesMVVM project

---
+++ challenges/namesMVVM/namesMVVM/namesMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  namesMVVMApp.swift
+//  namesMVVM
+//
+//  Created by gülçin çetin on 9.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct namesMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
